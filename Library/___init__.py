53da375a36c89277b4793d649ab52397757ceb3f
mrpqzm <45233889+mrpqzm@users.noreply.github.com>
2019-03-21 13:49:56 +0800

    Delete ___init__.py

--- Library/___init__.py
+++
@@ -1,9 +0,0 @@
-
-pkgname = 'urlwatch'
-
-__copyright__ = 'Copyright 2008-2019 Thomas Perl'
-__author__ = 'Thomas Perl <m@thp.io>'
-__license__ = 'BSD'
-__url__ = 'https://thp.io/2008/urlwatch/'
-__version__ = '2.16'
-__user_agent__ = '%s/%s (+https://thp.io/2008/urlwatch/info.html)' % (pkgname, __version__)
